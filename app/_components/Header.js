3c60fb67961baac2f2950c85cb36e55509607868
sanjaraiy <deepakrai78788@gmail.com>
2024-07-16 12:30:05 +0530

    added authontication

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -53,7 +53,7 @@ function Header() {
           <div class="sm:flex sm:gap-4">
             <a
               class="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700 dark:hover:bg-blue-700"
-              href="#"
+              href="/files"
             >
               Get Started
             </a>
