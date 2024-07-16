544d06ed357d6d5c7c99e2965b42f0852b60f1f4
sanjaraiy <deepakrai78788@gmail.com>
2024-07-16 10:16:25 +0530

    added Header and Hero section

---
+++ app/_components/Header.js
@@ -0,0 +1,84 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <header class="bg-white dark:bg-gray-900">
+    <div class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8 border-b">
+    <Image src='/logo.svg' width={109} height={43}></Image>
+  
+      <div class="flex flex-1 items-center justify-end md:justify-between">
+        <nav aria-label="Global" class="hidden md:block">
+          <ul class="flex items-center gap-6 text-sm">
+            <li>
+              <a
+                class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+  
+            <li>
+              <a
+                class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                href="#"
+              >
+                Upload
+              </a>
+            </li>
+  
+            <li>
+              <a
+                class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                href="#"
+              >
+                About us
+              </a>
+            </li>
+  
+            <li>
+              <a
+                class="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                href="#"
+              >
+                Contact us
+              </a>
+            </li>
+  
+          </ul>
+        </nav>
+  
+        <div class="flex items-center gap-4">
+          <div class="sm:flex sm:gap-4">
+            <a
+              class="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700 dark:hover:bg-blue-700"
+              href="#"
+            >
+              Get Started
+            </a>
+           </div>
+  
+          <button
+            class="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
+          >
+            <span class="sr-only">Toggle menu</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
+  )
+}
+
+export default Header
